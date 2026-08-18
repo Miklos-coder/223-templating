f1a33d99b27f1464159943fb72effb31a4ab9d2a
Miklos-coder <sonofmiki@gmail.com>
2026-08-18 19:51:47 +0200

    feat: complete ejs tutorial

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
